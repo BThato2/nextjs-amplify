b3341762810bb76953f368a38bf13318d104153a
BThato2 <123950579+BThato2@users.noreply.github.com>
2023-12-07 16:19:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+//commited on github 
+  
   return (
     <div className={styles.container}>
       <Head>
